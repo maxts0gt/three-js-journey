dbd6f4bcd3ceec985ca6979c766749a81e7ae540
Max <bazarsadtsogtjargal@gmail.com>
2022-01-18 20:15:39 +0900

    complete animation lesson

--- 06-animations/src/script.js
+++ 06-animations/src/script.js
@@ -1,6 +1,8 @@
 import './style.css';
 import * as THREE from 'three';
+import gsap from 'gsap';
 
+console.log(gsap);
 // Canvas
 const canvas = document.querySelector('canvas.webgl');
 
@@ -29,4 +31,43 @@ const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
 renderer.setSize(sizes.width, sizes.height);
-renderer.render(scene, camera);
+
+// Clock
+const clock = new THREE.Clock();
+
+// Time
+let time = Date.now();
+
+gsap.to(mesh.position, {
+  x: 2,
+  duration: 1,
+  delay: 1,
+});
+
+gsap.to(mesh.position, {
+  x: 0,
+  duration: 1,
+  delay: 2,
+});
+
+// Animations
+const tick = () => {
+  // Clock
+  //   const elapsedTime = clock.getElapsedTime();
+  //   //   console.log(elapsedTime);
+  //   // Time - to run it at same rate on any machine & browser
+  //   const currentTime = Date.now();
+  //   const deltaTime = currentTime - time;
+  //   time = currentTime;
+
+  //   //   Update objects
+  //   mesh.position.y = Math.sin(elapsedTime);
+  //   mesh.position.x = Math.cos(elapsedTime);
+  //   camera.lookAt(mesh.position);
+
+  //   Render
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(tick);
+};
+
+tick();
